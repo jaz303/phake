11b96bfb16bd482d16444fb60278117887ec0478
Think Wu <yingxinwu.g@gmail.com>
2012-01-08 15:37:23 +0800

    增加halt方法, 方便脚本控制build的失败情况

--- lib/utils.php
+++ lib/utils.php
@@ -1,37 +1,50 @@
 <?php
-namespace phake;
+namespace phake {
 
-function resolve_runfile($directory) {
-    $runfiles = array('Phakefile', 'Phakefile.php');
-    do {
-        foreach ($runfiles as $r) {
-            $candidate = $directory . '/' . $r;
-            if (file_exists($candidate)) {
-                return $candidate;
+    function resolve_runfile($directory) {
+        $runfiles = array('Phakefile', 'Phakefile.php');
+        do {
+            foreach ($runfiles as $r) {
+                $candidate = $directory . '/' . $r;
+                if (file_exists($candidate)) {
+                    return $candidate;
+                }
             }
-        }
-        if ($directory == '/') {
-            throw new \Exception("No Phakefile found");
-        }
-        $directory = dirname($directory);
-    } while (true);
-}
-
-function load_runfile($file) {
-    require $file;
-}
-
-function fatal($exception, $message = null) {
-    echo "aborted!\n";
-    if (!$message) $message = $exception->getMessage();
-    if (!$message) $message = get_class($exception);
-    echo $message . "\n\n";
-    global $trace;
-    if ($trace) {
-        echo $exception->getTraceAsString() . "\n";
-    } else {
-        echo "(See full trace by running task with --trace)\n";
+            if ($directory == '/') {
+                throw new \Exception("No Phakefile found");
+            }
+            $directory = dirname($directory);
+        } while (true);
     }
-    die(1);
+
+    function load_runfile($file) {
+        require $file;
+    }
+
+    function fatal($exception, $message = null) {
+        echo "aborted!\n";
+        if (!$message) $message = $exception->getMessage();
+        if (!$message) $message = get_class($exception);
+        echo $message . "\n\n";
+        global $trace;
+        if ($trace) {
+            echo $exception->getTraceAsString() . "\n";
+        } else {
+            echo "(See full trace by running task with --trace)\n";
+        }
+        die(1);
+    }
+}
+
+namespace {
+
+    /**
+     * Halt the build, throwing an exception
+     * @param m the error message
+     */
+    function halt($m) {
+        throw new Exception($m);
+    }
+
 }
 ?>
